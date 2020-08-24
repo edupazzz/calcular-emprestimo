bd99dd859c34df0e009c629d4026f9c0da39ec20
Eduardo Paz <du.passos51@gmail.com>
2020-08-24 14:57:10 -0300

    Terminando gráficos canvas

--- javascript/functions.js
+++ javascript/functions.js
@@ -9,13 +9,13 @@
 
   function calculate()
   {
-        var amount = document.getElementById('amount');
-        var apr = document.getElementById('apr');
-        var years = document.getElementById('years');
-        var zipcode = document.getElementById('zipcode');
-        var payment = document.getElementById('payment');
-        var total = document.getElementById('total');
-        var totalinterest = document.getElementById('totalinterest');
+        var amount = document.getElementById("amount");
+        var apr = document.getElementById("apr");
+        var years = document.getElementById("years");
+        var zipcode = document.getElementById("zipcode");
+        var payment = document.getElementById("payment");
+        var total = document.getElementById("total");
+        var totalinterest = document.getElementById("totalinterest");
 
      /*Obtém entrada do usuário através dos elementos de entrada.
         Presume que tudo isso é válido;
@@ -26,20 +26,20 @@
     
         var principal = parseFloat(amount.value);
         var interest = parseFloat(apr.value) / 100 / 12;
-        var payments = parseFloat(payment.value) * 12;
+        var payments = parseFloat(years.value) * 12;
 
         //agora calcula o valor do pagamento mensal.
         var x = Math.pow(1 + interest, payments);
 
-        var monthly = (principal*x*interest)/(x-1);
+        var monthly = (principal * x * interest) / (x-1);
         
         if (isFinite(monthly))
         {
             //Aqui, preenchemos os campos de saída, com duas casas decimais:
 
-            payment.innerHTML = monthly.toFixed(2);
-            total.innerHTML = (monthly*payments).toFixed(2);
-            totalinterest.innerHTML = ((monthly*payments)-principal).toFixed(2);
+          payment.innerHTML = monthly.toFixed(2);
+          total.innerHTML = (monthly * payments).toFixed(2);
+          totalinterest.innerHTML = ((monthly*payments)-principal).toFixed(2);
 
             //Agora vamos salvar as entradas do usuário,
             //para que possamos recuperá-las na próxima vez que ele entrar
@@ -48,9 +48,9 @@
 
             try //capturar quaisquer erros que ocorram dentro destas chaves:
             {
-                getLenders(amount.value, apr.value, years.value, zipcode.value);
+              getLenders(amount.value, apr.value, years.value, zipcode.value);
             }
-            catch(e) {/*e ignora esses erros...*/}
+            catch(e) {/*e ignora esses erros...*/} 
 
             chart(principal, interest, monthly, payments);
         }
@@ -59,12 +59,21 @@
             /*Se o resultado for NaN ou infinito: entrada inválida! 
               Qualquer saída exibida anteriormente é apagada*/
 
-              payment.innerHTML = "";
+            payment.innerHTML = "";
             total.innerHTML = "";
             totalinterest.innerHTML = "";
             chart(); //Apaga o gŕafico;
         }
   }
+
+  function save(amount, apr, years, zipcode) {
+    if (window.localStorage) {
+        localStorage.loan_amount = amount;
+        localStorage.loan_apr = apr;
+        localStorage.loan_years = years;
+        localStorage.loan_zipcode = zipcode;
+    }
+}
 
   //Aqui tentamos restaurar os campos de entrada quando o ducumento é carregado pela primeira vez.
 
@@ -106,5 +115,67 @@
     g.fillStyle = "#f88";
     g.fill();
     g.font = "bold 12px sans-serif";
-    g.fillText(" bla bláblá");
+    g.fillText("Total de Juros Pagos", 20, 20); //Desenha texto na legenda;
+
+
+    var equity = 0; //Inicia uma nova figura, começando no canto inferior esquerdo;
+    g.beginPath();
+    g.moveTo(paymentToX(0), amountToY(0));
+
+    for (var p=1; p<=payments; p++)
+    {//Para cada pagamento, descobre quando é o juro
+      var thisMonthsInterest = (principal-equity)*interest;
+      equity += (monthly - thisMonthsInterest);
+      g.lineTo(paymentToX(p), amountToY(equity));
+    }
+
+    g.lineTo((payments), amountToY(0));
+    g.closePath();
+    g.fillStyle = "green";
+    g.fill();
+    g.fillText("Total Equity", 20, 35);
+
+    //Saldo devedor
+    var bal = principal;
+    g.beginPath();
+    g.moveTo(paymentToX(p), amountToY(bal));
+
+    for (var p=1; p<=payments; p++)
+    {
+      var thisMonthsInterest = bal * interest;
+      bal -= (monthly - thisMonthsInterest);
+      g.lineTo(paymentToX(p), amountToY(bal));
+    }
+
+    g.lineWidth = 3;
+    g.stroke();
+    g.fillStyle = "black";
+    g.fillText("Loan Balance", 20,50);
+
+    //marcações anuaise os números do ano
+    g.textAlign = "center";
+
+    var y = amountToY(0);
+
+    for(var year=1; year*12<= payments; year++)
+    {
+      var x = paymentToX(year*12);
+      g.fillRect(x-0.5, y-3,1,3);
+
+      if (year == 1) g.fillText("Ano", x, y-5);
+      if (year % 5 == 0 && year*12 !== payments)
+        g.fillText(String(year), x, y-5);
+    }
+
+    g.textAlign = "right";                         
+    g.textBaseline = "middle";                    
+    var ticks = [monthly*payments, principal];   
+    var rightEdge = paymentToX(payments);  
+    for(var i = 0; i < ticks.length; i++)
+    {
+        var y = amountToY(ticks[i]);
+        g.fillRect(rightEdge-3, y-0.5, 3,1);
+        g.fillText(String(ticks[i].toFixed(0)),
+                   rightEdge-5, y);
+    }
   }
